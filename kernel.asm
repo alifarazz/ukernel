074bce703620fa276b68bdfb84a694e128bf8f50
Ali Farazdaghi <alifarazdaghi@gmail.com>
2017-12-06 16:33:49 +0330

    Uncomment funcions

--- kernel.asm
+++ kernel.asm
@@ -17,7 +17,7 @@ global keyboard_handler
 
 ;defined in the C file
 extern kmain			
-;; extern keyboard_handler_main
+extern keyboard_handler_main
 	
 read_port:
 	mov edx, [esp + 4]
@@ -37,7 +37,7 @@ load_idt:
 	ret
 
 keyboard_handler:
-	;; call keyboard_handler_main
+	call keyboard_handler_main
 	iretd	
 		
 start:
@@ -49,4 +49,4 @@ start:
 section .bss
 	resb 8192		;8kb for stack
 stack_space:
-;;	lol!
+;;	well...
